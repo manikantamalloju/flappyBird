298b07034e7024b82c3a3be0359f42e697350d27
manikanta malloju <manikanta.malloju@gmail.com>
2024-07-24 20:06:42 +0530

    baseSetUpBackground

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import React from "react";
+import { Canvas, useImage, Image } from "@shopify/react-native-skia";
+import { useWindowDimensions } from "react-native";
 
-export default function App() {
+const App = () => {
+  const { width, height } = useWindowDimensions();
+  const bg = useImage(require("./assets/sprites/background-day.png"));
+  const bird = useImage(require("./assets/sprites/yellowbird-upflap.png"));
+  const pipeBottom = useImage(require("./assets/sprites/pipe-green.png"));
+  const topPipe = useImage(require("./assets/sprites/pipe-green - top.png"));
+  const base = useImage(require("./assets/sprites/base.png"));
+  const pipeOffset = 0;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <Canvas style={{ width, height }}>
+      {/* BG */}
+      <Image image={bg} fit={"cover"} width={width} height={height} />
+      <Image
+        image={base}
+        width={width}
+        height={150}
+        y={height - 75}
+        x={0}
+        fit={"cover"}
+      />
+      {/* Pipe  */}
+      <Image
+        image={topPipe}
+        y={pipeOffset - 320}
+        x={width / 2}
+        width={103}
+        height={640}
+      />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <Image
+        image={pipeBottom}
+        y={height - 320 + pipeOffset}
+        x={width / 2}
+        width={103}
+        height={640}
+      />
+
+      {/* Bird */}
+      <Image image={bird} y={height / 2} x={width / 4} width={64} height={48} />
+    </Canvas>
+  );
+};
+export default App;
