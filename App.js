cf9049378c5b06714154a15e9c58017e78c9cb19
manikanta malloju <manikanta.malloju@gmail.com>
2024-07-24 21:30:25 +0530

    moving_pipes

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 // import React from "react";
+import { useEffect } from "react";
 import { Canvas, useImage, Image } from "@shopify/react-native-skia";
 import { useWindowDimensions } from "react-native";
+import {
+  useSharedValue,
+  withTiming,
+  Easing,
+  withSequence,
+  withRepeat,
+} from "react-native-reanimated";
 
 const App = () => {
   const { width, height } = useWindowDimensions();
@@ -9,11 +17,34 @@ const App = () => {
   const pipeBottom = useImage(require("./assets/sprites/pipe-green.png"));
   const topPipe = useImage(require("./assets/sprites/pipe-green - top.png"));
   const base = useImage(require("./assets/sprites/base.png"));
+  const x = useSharedValue(width);
+  useEffect(() => {
+    x.value = withRepeat(
+      withSequence(
+        withTiming(-150, {
+          duration: 3000,
+          easing: Easing.linear,
+        }),
+        withTiming(width, { duration: 0 })
+      ),
+      -1
+    );
+  }, []);
   const pipeOffset = 0;
+
   return (
-    <Canvas style={{ width, height }}>
+    <Canvas
+      style={{ width, height }}
+      onTouch={() =>
+        (x.value = withTiming(-100, {
+          duration: 3000,
+          easing: Easing.linear,
+        }))
+      }
+    >
       {/* BG */}
       <Image image={bg} fit={"cover"} width={width} height={height} />
+      {/* Base  */}
       <Image
         image={base}
         width={width}
@@ -26,7 +57,7 @@ const App = () => {
       <Image
         image={topPipe}
         y={pipeOffset - 320}
-        x={width / 2}
+        x={x}
         width={103}
         height={640}
       />
@@ -34,7 +65,7 @@ const App = () => {
       <Image
         image={pipeBottom}
         y={height - 320 + pipeOffset}
-        x={width / 2}
+        x={x}
         width={103}
         height={640}
       />
